5ed2a4f97075be29551bf82241c88529a6eebd54
LoskStirka <snegoweek12@yandex.ru>
2022-11-06 18:26:23 +0300

    feat(lw0): uploaded fizzbuzz.dart

---
+++ lw0/fizzbuzz.dart
@@ -0,0 +1,21 @@
+void main() {
+	fizzbuzz(15);
+}
+
+void fizzbuzz(int n) {
+	for (int i = 1; i < n; i++) {
+		print(i);
+		if (i % 3 == 0) {
+			print("fizz");
+			i++;
+		}
+		if (i % 5 == 0) {
+			print("buzz");
+			i++;
+		}
+		if (i % 3 == 0 && i % 5 == 0) {
+			print("fizzbuzz");
+			i++;
+		}
+	}
+}
